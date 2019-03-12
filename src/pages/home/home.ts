c2f203aa3e0904055babe93b68f69888261f3cf3
Alejandra Aravena <janaravena@gmail.com>
2019-03-12 11:11:41 -0300

    paso 5

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,6 +15,8 @@ export class HomePage {
   @ViewChild('mapa') mapaElement: ElementRef;
   map: any;
 
+  markers = [];
+  
   constructor(
     public navCtrl: NavController,
     public platform: Platform,
@@ -27,13 +29,52 @@ export class HomePage {
   }
 
   initMap() {
-    this.map = new google.maps.Map(this.mapaElement.nativeElement, {
-      zoom: 13,
-      center: {
-        lat: -33.4378886, 
-        lng: -70.6526897
-      }
+    this.geolocation.getCurrentPosition({ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }).then((resp) => {
+      let miPosicionInicial = new google.maps.LatLng(resp.coords.latitude,resp.coords.longitude);
+      this.map = new google.maps.Map(this.mapaElement.nativeElement, {
+        zoom: 15,
+        center: miPosicionInicial
+      });
     });
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      this.deleteMarkers();
+      let miPosicionActual = new google.maps.LatLng(data.coords.latitude,data.coords.longitude);
+      this.addMarker(miPosicionActual);
+      this.setMapOnAll(this.map);
+    });
+  }
+
+  addMarker(location) {
+    let marker = new google.maps.Marker({
+      position: location,
+      map: this.map,
+      icon: {
+        url: 'assets/logo.png',
+        size: new google.maps.Size(126, 75),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 8),
+        scaledSize: new google.maps.Size(32, 19)
+      },
+      //title: 'Identificador',
+			//animation: 'DROP',
+    });
+    this.markers.push(marker);
+  }
+  
+  setMapOnAll(map) {
+    for (var i = 0; i < this.markers.length; i++) {
+      this.markers[i].setMap(map);
+    }
+  }
+  
+  clearMarkers() {
+    this.setMapOnAll(null);
+  }
+  
+  deleteMarkers() {
+    this.clearMarkers();
+    this.markers = [];
   }
 
 }
