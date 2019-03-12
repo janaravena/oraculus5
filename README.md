835cd8f1e78f09b5b69939b34e6604595075b72f
Alejandra Aravena <janaravena@gmail.com>
2019-03-12 01:40:20 -0300

    paso 2

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 
 ## Ejercicios GOOGLE MAPS JAVASCRIPT
 
-### 1)Crear una app con tabs.
+### 1)Crear una nueva aplicación con tabs.
 ```
 $ ionic start modulo5 tabs --type=ionic-angular --cordova
 
 $ cd modulo5
 ```
+
+### 2)Agregar Firebase al proyecto.
+```
+$ npm install --save firebase
+```
+
+### 3)Agregar Google Maps Javascript.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDhBiNm7I3b9tX_g7g0iDvzwEdy9LXV3ZI",
+    authDomain: "oraculus5-ale-faef0.firebaseapp.com",
+    databaseURL: "https://oraculus5-ale-faef0.firebaseio.com",
+    projectId: "oraculus5-ale-faef0",
+    storageBucket: "oraculus5-ale-faef0.appspot.com",
+    messagingSenderId: "24531287229"
+};
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -18,5 +29,7 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    firebase.initializeApp(config);
   }
 }
